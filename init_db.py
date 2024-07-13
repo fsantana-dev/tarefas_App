d5c8343325a90b68e89c8d9c561d10943abb0854
Francisco <bragasan1@yahoo.com.br>
2024-07-13 14:12:10 -0400

    Initial commit

--- init_db.py
+++ init_db.py
@@ -1,26 +1,27 @@
 import sqlite3
 
-conn = sqlite3.connect('tasks.db')
-cursor = conn.cursor()
+def init_db():
+    conn = sqlite3.connect('tasks.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS usuarios (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            email TEXT NOT NULL UNIQUE,
+            senha TEXT NOT NULL
+        )
+    ''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS tarefas (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            userId INTEGER,
+            tarefa TEXT NOT NULL,
+            dataVencimento TEXT NOT NULL,
+            concluida BOOLEAN NOT NULL,
+            FOREIGN KEY (userId) REFERENCES usuarios (id)
+        )
+    ''')
+    conn.commit()
+    conn.close()
 
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS usuarios (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email TEXT NOT NULL,
-    senha TEXT NOT NULL
-)
-''')
-
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS tarefas (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    userId INTEGER NOT NULL,
-    tarefa TEXT NOT NULL,
-    dataVencimento TEXT NOT NULL,
-    concluida BOOLEAN NOT NULL DEFAULT 0,
-    FOREIGN KEY (userId) REFERENCES usuarios(id)
-)
-''')
-
-conn.commit()
-conn.close()
+if __name__ == '__main__':
+    init_db()
